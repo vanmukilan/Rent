1572c40441d841add6408fda9d9803dbaf8b95f8
vanmukilan.R VANMUKILAN.R <142682590+vanmukilan@users.noreply.github.com>
2023-11-17 12:37:13 +0530

    Update Footer.jsx

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
+        <span>© 2021 RentUP. Design By Mukilan .</span>
       </div>
     </>
   )
